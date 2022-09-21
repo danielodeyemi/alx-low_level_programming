d87eb71935ec6b03f10db79e161c229bf22f7347
danielodeyemi <odeyemi7876@run.edu.ng>
2022-09-21 08:40:23 -0700

    Fixed timestable to betty styling

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
-			
+
 			if ((n / 10) == 0)
 			{
 				if (j != 0)
@@ -28,7 +28,7 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((n /10) + '0');
+				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
