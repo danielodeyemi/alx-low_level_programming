ab0be917246d3107d252fe7415c90f52442540f1
Daniel Odeyemi Abyaomi <63434990+danielodeyemi@users.noreply.github.com>
2022-09-21 22:37:25 +0100

    added files for 0x06 one to seven

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase letters of a  string
+ * to upper case
+ * @s: an input string
+ * Return: char pointer to converted string
+ */
+char *string_toupper(char *str)
+{
+	char *start = str;
+
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str -= 32;
+		s++;
+	}
+	return (start);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+int check_seperators(char c);
+
+/**
+ * cap_string - a function that capitalizes all words of a string.
+ * @str: An input string to capitalize letters
+ * Return: pointer to s
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+
+		if (check_seperators(str[i]) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			str[i + 1] -= 32;
+		i++;
+	}
+
+	return (str);
+}
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+}
