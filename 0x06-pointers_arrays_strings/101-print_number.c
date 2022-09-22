1cdb8a0e37c7faa9a9f619662413286b82a1a23b
Daniel Odeyemi Abyaomi <63434990+danielodeyemi@users.noreply.github.com>
2022-09-22 03:52:35 +0100

    cleaned up code and fixed style

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,48 +1,35 @@
 #include "main.h"
 
 /**
- * base10 - power in 10 base
- * print_number - prints integers entered using putchar
- * @n: an exponent
- * Return: returns 10 to power exponent
+ * print_number - prints integers entered using putchar.
+ * @n: integer.
+ * Return: returns (success).
  */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n *= -1;
+		_putchar(45);
+		m = n * -1;
 	}
-
-	if (n == 0)
-		_putchar('0');
-
 	else
 	{
-		while (n / power == 0)
-			power /= 10;
+		m = n;
+	}
 
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
 	}
 }
